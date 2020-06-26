d8c8b6928d9e28f5d5fd5fe5e014001dd6e30de0
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-26 06:42:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  const arr = array
-  arr.unshift(element)
-  return arr
+  return [element,...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {

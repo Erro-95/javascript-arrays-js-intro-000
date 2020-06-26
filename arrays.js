7a2592e5e835d897b9c1d54fd3659fdb3bb4ea93
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-26 05:59:45 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+const chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function addElementToBeginningOfArray(a, b) {
+  return [b,...a]
+}
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+  return a.unshift(b)
+}
+
+function addElementToEndOfArray(a, b) {
+
+}
+
+function destructivelyAddElementToEndOfArray(a, b) {
+
+}
